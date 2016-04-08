e64ef9a38007e3e6dc16ca1e1a87bc597f30eb08
Christian <ravenblack@live.de>
2016-04-08 16:50:44 +0200

    Gradle settings

--- app/src/main/java/go4/szut/de/nametrainer/selection/SelectionActivity.java
+++ app/src/main/java/go4/szut/de/nametrainer/selection/SelectionActivity.java
@@ -1,17 +1,13 @@
 package go4.szut.de.nametrainer.selection;
 
-import android.app.Activity;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
 
 import go4.szut.de.nametrainer.R;
 
