f4a074e3c080d04addcfe0ab9f1c3cb6d3d4a6a5
Christian <ravenblack@live.de>
2016-04-08 14:18:39 +0200

    Toolbar added and a simple Design

--- app/src/main/java/go4/szut/de/nametrainer/options/OptionsActivity.java
+++ app/src/main/java/go4/szut/de/nametrainer/options/OptionsActivity.java
@@ -2,26 +2,32 @@ package go4.szut.de.nametrainer.options;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.widget.CompoundButton;
-import android.widget.ToggleButton;
+import android.widget.Switch;
 
 import go4.szut.de.nametrainer.R;
 
 /**
  * Created by Michele on 24.03.2016.
  */
-public class OptionsActivity extends Activity {
+public class OptionsActivity extends AppCompatActivity {
 
     private boolean soundEnabled;
     private boolean lastNameEnabled;
-    private ToggleButton soundButton;
-    private ToggleButton lastNameButton;
+    private Switch soundButton;
+    private Switch lastNameButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
-        soundButton = (ToggleButton) findViewById(R.id.toggleButtonSound);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        soundButton = (Switch) findViewById(R.id.toggleButtonSound);
         soundEnabled = soundButton.isChecked();
         soundButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -32,7 +38,7 @@ public class OptionsActivity extends Activity {
                 }
             }
         });
-        lastNameButton = (ToggleButton) findViewById(R.id.toggleButtonLastName);
+        lastNameButton = (Switch) findViewById(R.id.toggleButtonLastName);
         lastNameEnabled= lastNameButton.isChecked();
         lastNameButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
